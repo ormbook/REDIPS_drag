c3645ee203c4078203862547cb19c2702c753ce9
ormbook <ormbook@gmail.com>
2017-02-28 11:30:39 +0700

    Update places.inc.php

--- ormTravel/places.inc.php
+++ ormTravel/places.inc.php
@@ -1,14 +1,36 @@
+<?php if($param=="Home"){ ?>
+<div class="redips-drag redips-clone ar">Get up</div>
+<div class="redips-drag redips-clone ar">Bath</div>
+<div class="redips-drag redips-clone ar">Breakfast</div>
+<div class="redips-drag redips-clone ar">Pack Bag</div>
+<div class="redips-drag redips-clone ar">Check in</div>
+<div class="redips-drag redips-clone ar">Check out</div>
+<div class="redips-drag redips-clone ar">Check Passport</div>
+<div class="redips-drag redips-clone ar">Airport</div>
+<div class="redips-drag redips-clone ar">Fly</div>
+<div class="redips-drag redips-clone ar">Go</div>
+<div class="redips-drag redips-clone ch">Welcome</div>
+<div class="redips-drag redips-clone bi">Booking</div>
+<div class="redips-drag redips-clone bi">Flight</div>
+<div class="redips-drag redips-clone bi">Hotel</div>
+<div class="redips-drag redips-clone bi">Train</div>
+
+<?php
+}else{
+?>
 <table>
 <tbody>
-<tr><td class="dark"><div id="ar" class="redips-drag redips-clone ar">Arts</div></td></tr>
-<tr><td class="dark"><div id="bi" class="redips-drag redips-clone bi">Biology</div></td></tr>
-<tr><td class="dark"><div id="ch" class="redips-drag redips-clone ch">Chemistry</div></td></tr>
-<tr><td class="dark"><div id="en" class="redips-drag redips-clone en">English</div></td></tr>
-<tr><td class="dark"><div id="et" class="redips-drag redips-clone et">Ethics</div></td></tr>
-<tr><td class="dark"><div id="hi" class="redips-drag redips-clone hi">History</div></td></tr>
-<tr><td class="dark"><div id="it" class="redips-drag redips-clone it">IT</div></td></tr>
-<tr><td class="dark"><div id="ma" class="redips-drag redips-clone ma">Mathematics</div></td></tr>
-<tr><td class="dark"><div id="ph" class="redips-drag redips-clone ph">Physics</div></td></tr>
-
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Arts</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Biology</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ch">Chemistry</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone en">English</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone et">Ethics</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone hi">History</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone it">IT</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ma">Mathematics</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ph">Physics</div></td></tr>
 </tbody>
 </table>
+<?php
+}//end if
+?>

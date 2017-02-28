35ac4b88a5dd2184cf0b2bf529951cd6a3f8abe3
ormbook <ormbook@gmail.com>
2017-02-28 13:51:57 +0700

    Update places.inc.php

--- ormTravel/places.inc.php
+++ ormTravel/places.inc.php
@@ -17,17 +17,30 @@
 <tr><td class="dark redips-single"><div class="redips-drag redips-clone bi">Hotel</div></td></tr>
 <tr><td class="dark redips-single"><div class="redips-drag redips-clone bi">Train</div></td></tr>
 <?php
-}else{
+}else if($param=="Highlight"){
 ?>
-<tr><td class="dark"><div class="redips-drag redips-clone ar">Arts</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone bi">Biology</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone ch">Chemistry</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone en">English</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone et">Ethics</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone hi">History</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone it">IT</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone ma">Mathematics</div></td></tr>
-<tr><td class="dark"><div class="redips-drag redips-clone ph">Physics</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Disney Land</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Disney Sea</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ch">อุเอโนะ (Ueno)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone en">ชินจูกุรุ (Shinjuku)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone et">โอไดบะ (Odaiba)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone hi">นิกโก้ (Nikko)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone it">ฟูจิซัง (Fuji Mt.)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ma">คาวาคูจิโกะ (Kawakuchiko)</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ph">ฮาโกเน่ (Hakone)</div></td></tr>
+
+ <?php
+}else if($param=="Transport"){
+?>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">เดินทางนาริตะ-เข้าเมือง</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">เดินทางฮาเนดะ-เข้าเมือง</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ch">เดินทางไปสนามบินนาริตะ</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone en">เดินทางไปสนามบินฮาเนดะ</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone et">เดินทางไปภูเขาไฟฟูจิ</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone hi">เดินทางไปฮาโกเน่</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone it">เดินทางไป...</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ma">JR</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ph">KEISE</div></td></tr>
 <?php
 }//end if
 ?>

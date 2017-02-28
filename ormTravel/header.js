bcb7815d062762b4aef7158a1ec22c529897b2be
ormbook <ormbook@gmail.com>
2017-02-28 16:39:21 +0700

    Update header.js

--- ormTravel/header.js
+++ ormTravel/header.js
@@ -7,7 +7,7 @@
 // define headerInit and default indexURL / redipsURL variable
 var headerInit,
 	redipsURL = redipsURL || '/javascript/drag-and-drop-table-content/',
-	indexURL = indexURL || '../';
+	indexURL = indexURL || '/ormTravel';
 
 // header initialization
 headerInit = function () { 
@@ -16,10 +16,10 @@ headerInit = function () {
 	// add "header" DIV element
 	document.body.insertBefore(header, document.body.firstChild);
 	// apply inner HTML
-	//header.innerHTML = '<div style="background-color:#eee;padding:10px;text-align:center;font-size:20px;font-weight:bold">' + title + '</div>' +
-		'<div style="float:left;width:50%;padding-left:10px"><a href="' + indexURL + '">Index</a></div>' +
-		'<div style="text-align:right;padding-right:10px;margin-bottom:10px"><a href="http://www.redips.net' + redipsURL + '">www.redips.net</a></div>';
-    header.innerHTML ='';
+	header.innerHTML = '<div style="background-color:#eee;padding:10px;text-align:center;font-size:20px;font-weight:bold">' + title + '</div>' +
+		'<div style="float:left;width:50%;padding-left:10px"><a href="' + indexURL + '">Home</a></div>' +
+		'<div style="text-align:right;padding-right:10px;margin-bottom:10px"><a href="http://www.ormtravel.com">www.ormtravel.com</a></div>';
+    
 };
 
 // add onload event listener

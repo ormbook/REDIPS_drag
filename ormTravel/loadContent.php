9adb490b001d38d39a1abd0eb63f8c2ca9b19cdf
ormbook <ormbook@gmail.com>
2017-02-28 16:24:02 +0700

    Update loadContent.php

--- ormTravel/loadContent.php
+++ ormTravel/loadContent.php
@@ -1,6 +1,10 @@
 <?php
     //$url = 'http://www.anothersite.com';
-$url=$_REQUEST['url'];
-    $htm = file_get_contents($url);
-    echo $htm;
+$url=$_REQUEST['id'];       
+
 ?>
+<!-- 16:9 aspect ratio -->
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="<?php echo $url;?>"></iframe>
+</div>
+

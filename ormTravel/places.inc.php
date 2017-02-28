8113bbc5510549496a7a166dd7f537b8dc9f815d
ormbook <ormbook@gmail.com>
2017-02-28 11:34:51 +0700

    Update places.inc.php

--- ormTravel/places.inc.php
+++ ormTravel/places.inc.php
@@ -1,20 +1,23 @@
 <?php if($param=="Home"){ ?>
-<div class="redips-drag redips-clone ar">Get up</div>
-<div class="redips-drag redips-clone ar">Bath</div>
-<div class="redips-drag redips-clone ar">Breakfast</div>
-<div class="redips-drag redips-clone ar">Pack Bag</div>
-<div class="redips-drag redips-clone ar">Check in</div>
-<div class="redips-drag redips-clone ar">Check out</div>
-<div class="redips-drag redips-clone ar">Check Passport</div>
-<div class="redips-drag redips-clone ar">Airport</div>
-<div class="redips-drag redips-clone ar">Fly</div>
-<div class="redips-drag redips-clone ar">Go</div>
-<div class="redips-drag redips-clone ch">Welcome</div>
-<div class="redips-drag redips-clone bi">Booking</div>
-<div class="redips-drag redips-clone bi">Flight</div>
-<div class="redips-drag redips-clone bi">Hotel</div>
-<div class="redips-drag redips-clone bi">Train</div>
-
+<table>
+<tbody>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Get up</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Bath</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Breakfast</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Pack Bag</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Check in</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Check out</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Check Passport</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Airport</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Fly</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ar">Go</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone ch">Welcome</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Booking</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Flight</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Hotel</div></td></tr>
+<tr><td class="dark"><div class="redips-drag redips-clone bi">Train</div></td></tr>
+</tbody>
+</table>
 <?php
 }else{
 ?>

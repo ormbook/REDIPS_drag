b03541b2473080edddb326b40a206df5274af2d6
ormbook <ormbook@gmail.com>
2017-03-01 14:39:06 +0700

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -3,4 +3,11 @@
 $dir    = '/';
 $files1 = scandir($dir);
 print_r($files1);
+
+for($i=0;$i<count($files1);$i++){
+  $dir    = '/'.$files1[$i];
+  $files2 = scandir($dir);
+  print_r($files2);
+  echo "<br>";
+}
 ?>

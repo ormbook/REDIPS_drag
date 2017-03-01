f4afbe57d64a81d848d54aa5b09b9d7dfe753577
ormbook <ormbook@gmail.com>
2017-03-01 14:36:10 +0700

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,19 +1,6 @@
 <?php
 
-if ($handle = opendir('/')) {
-    echo "Directory handle: $handle\n";
-    echo "Entries:\n";
-
-    /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($handle))) {
-        echo "$entry\n";
-    }
-
-    /* This is the WRONG way to loop over the directory. */
-    while ($entry = readdir($handle)) {
-        echo "$entry\n";
-    }
-
-    closedir($handle);
-}
+$dir    = '/';
+$files1 = scandir($dir);
+print_r($files1);
 ?>

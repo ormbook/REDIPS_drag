0e9c29dd5e22c1b8356639485aa91b49bc26d807
ormbook <ormbook@gmail.com>
2017-03-01 14:47:30 +0700

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -5,7 +5,7 @@ $files1 = scandir($dir);
 print_r($files1);
 
 for($i=0;$i<count($files1);$i++){
-  $dir    = '/'.$files1[$i];
+  $dir    = '/app/'.$files1[$i];
   $files2 = scandir($dir);
   echo "<hr><b>".$dir."</b>";
   print_r($files2);
